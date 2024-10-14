135a6a5f668e2d1a434c406589b2548df8c285aa
Qifan Wu <1271067318@qq.com>
2024-10-15 03:04:07 +1100

    fix ball hit box by adding central position

--- src/main/java/inkball/entity/gameEntity/Ball.java
+++ src/main/java/inkball/entity/gameEntity/Ball.java
@@ -1,6 +1,7 @@
 package inkball.entity.gameEntity;
 
 
+import inkball.App;
 import inkball.entity.ImageCache;
 import inkball.entity.ImageEntity;
 import inkball.entity.CollideLine.MouseLine;
@@ -10,8 +11,8 @@ import java.util.List;
 
 public class Ball extends ImageEntity {
     private PVector velocity;
-    private double radious;
-
+    private final double radious;
+    private final PVector centerPosition;
     @Override
     public void draw() {
         position.add(velocity);
@@ -21,15 +22,24 @@ public class Ball extends ImageEntity {
 
     public Ball(int x, int y, Integer color, String name) {
         super(x, y, color, name);
-        this.velocity = new PVector(1, 1);
+        int randomX = Math.random() > 0.5 ? 2 : -2;
+        int randomY = Math.random() > 0.5 ? 2 : -2;
+        if (App.FPS == 60) {
+            randomX = randomX / 2;
+            randomY = randomY / 2;
+        }
+        this.velocity = new PVector(randomX, randomY);
         this.radious = 12;
+        this.centerPosition = new PVector((float) (position.x + radious),
+                (float) (position.y + radious));
     }
 
-    public void draw(MouseLine mouseLine,List<Wall> walls) {
+    public void draw(MouseLine mouseLine, List<Wall> walls) {
         app.image(ImageCache.ballsCache[color], position.x, position.y);
         this.checkMouseLine(mouseLine);
         this.checkWalls(walls);
         position.add(velocity);
+        centerPosition.add(velocity);
     }
 
     public void checkWalls(List<Wall> walls) {
@@ -41,18 +51,18 @@ public class Ball extends ImageEntity {
                 if (checkCollide(p1, p2)) {
                     //System.out.println("Wall Collose");
                     getCollide(p1, p2);
-                    int wallColor=wall.getColor();
-                    if (wallColor!=0) this.color = wallColor;
+                    int wallColor = wall.getColor();
+                    if (wallColor != 0) this.color = wallColor;
                     return;
                 }
             }
-            PVector p1 = points.get(0);
-            PVector p2 = points.get(points.size() - 1);
+            PVector p2 = points.get(0);
+            PVector p1 = points.get(points.size() - 1);
             if (checkCollide(p1, p2)) {
-               // System.out.println("Wall Collose");
+                // System.out.println("Wall Collose");
                 getCollide(p1, p2);
-                int wallColor=wall.getColor();
-                if (wallColor!=0) this.color = wallColor;
+                int wallColor = wall.getColor();
+                if (wallColor != 0) this.color = wallColor;
                 return;
             }
         }
@@ -74,11 +84,21 @@ public class Ball extends ImageEntity {
         }
     }
 
-    public boolean checkCollide(PVector p1,PVector p2){
-        double d1 = PVector.dist(p1, PVector.add(position, velocity));
-        double d2 = PVector.dist(p2, PVector.add(position, velocity));
-        double d3 = PVector.dist(p1, p2) + radious;
-        return d1 + d2 < d3;
+    public boolean checkCollide(PVector p1, PVector p2) {
+        PVector lineVec = PVector.sub(p2, p1);
+        PVector posVec = PVector.sub(centerPosition, p1);
+
+        // Project posVec onto lineVec
+        float projection = PVector.dot(posVec, lineVec) / lineVec.magSq();
+
+        // Clamp the projection to the range [0, 1] to ensure it's on the segment
+        projection = Math.max(0, Math.min(1, projection));
+
+        // Find the closest point on the line to the position
+        PVector closestPoint = PVector.add(p1, PVector.mult(lineVec, projection));
+
+        // Check if the distance from the ball's position to the closest point is less than its radius
+        return PVector.dist(centerPosition, closestPoint) < radious;
     }
 
 
@@ -86,8 +106,8 @@ public class Ball extends ImageEntity {
         PVector mid = PVector.add(p1, p2).div(2);
         PVector n1 = new PVector(p1.y - p2.y, p2.x - p1.x);
         PVector n2 = new PVector(p2.y - p1.y, p1.x - p2.x);
-        double checkDistance1 = PVector.dist(position, PVector.add(mid, n1));
-        double checkDistance2 = PVector.dist(position, PVector.add(mid, n2));
+        double checkDistance1 = PVector.dist(centerPosition, PVector.add(mid, n1));
+        double checkDistance2 = PVector.dist(centerPosition, PVector.add(mid, n2));
         PVector n;
         if (checkDistance1 > checkDistance2) {
             n = n1;
@@ -97,7 +117,6 @@ public class Ball extends ImageEntity {
         n.normalize();
         float v1 = 2 * PVector.dot(velocity, n);
         PVector v2 = PVector.mult(n, v1);
-        PVector u = PVector.sub(velocity, v2);
-        velocity = u;
+        velocity = PVector.sub(velocity, v2);
     }
 }
--- src/main/java/inkball/entity/gameEntity/Wall.java
+++ src/main/java/inkball/entity/gameEntity/Wall.java
@@ -19,18 +19,18 @@ public class Wall extends ImageEntity {
     public Wall(int x, int y) {
         super(x, y, 0, "wall");
         points.add(this.position);
+        points.add(new PVector(this.position.x+32, this.position.y));
+        points.add(new PVector(this.position.x+32, this.position.y+32));
         points.add(new PVector(this.position.x, this.position.y + 32));
-        points.add(new PVector(this.position.x + 32, this.position.y));
-        points.add(new PVector(this.position.x + 32, this.position.y + 32));
 
     }
 
     public Wall(int x, int y, int color, String name) {
         super(x, y, color, name);
         points.add(this.position);
+        points.add(new PVector(this.position.x+32, this.position.y));
+        points.add(new PVector(this.position.x+32, this.position.y+32));
         points.add(new PVector(this.position.x, this.position.y + 32));
-        points.add(new PVector(this.position.x + 32, this.position.y));
-        points.add(new PVector(this.position.x + 32, this.position.y + 32));
     }
 
     public int getColor() {
